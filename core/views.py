acefde9dbd72392b01d269a1659ba01c59749ec1
Malik <workcmtiayon@gmail.com>
2026-08-12 21:15:20 +0100

    Implement core views, dashboard route and application configuration

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def home_view(request):
+    return render(request, "core/home.html")
+
+
+@login_required
+def dashboard_view(request):
+    return render(request, "core/dashboard.html")
