7b30ae55b2c0442cb594c610774064856cc594a1
Malik <workcmtiayon@gmail.com>
2026-08-12 16:01:18 +0100

    feat: implement authentication flows and asynchronous email delivery with Celery, Redis and SMTP

---
+++ accounts/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .models import User
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Adresse E-mail")
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('Username', 'email')
+
+    def clean_email(self):
+        email = self.cleaned_data["email"].lower()
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError("Cette adresse e-mail est deja utilisee par un autre compte")
+        return email
+
+    class LoginForm(AuthenticationForm):
+        def confirm_login_allowed(self, user):
+            if not user.is_active:
+                raise forms.ValidationError(
+                    "Ce compte n'est pas encore active alors verifie ta boite email et cliquez sur le lien de confirmation",
+                    code="inactive",
+                )
+
+    class ForgotPasswordForm(forms.Form):
+        email = forms.EmailField(label='Adresse e-mail')
+        def clean_email(self):
+            email = self.cleaned_data["email"].lower()
+            if not User.objects.filter(email__iexact=email, is_active=True).exists():
+                raise forms.ValidationError("Aucun compte actif n'est associe a cette adress e-mail")
+            return email
---
+++ accounts/tasks.py
@@ -0,0 +1,53 @@
+from celery import shared_task
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+User = get_user_model()
+
+@shared_task(bind=True, max_retries=3, default_retry_delay=60)
+def send_activation_email_task(self, user_id, activation_link):
+
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return
+
+    subject = "Confirmez votre adresse Email"
+    message = render_to_string("accounts/emails/activation_email.txt", {"username" : user.username, "activation_link": activation_link})
+
+    try:
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
+    except Exception as exc:
+        raise self.retry(exc=exc)
+
+    
+
+@shared_task(bind=True, max_retries=3, default_retry_delay=60)
+def send_password_reset_email_task(self, user_id, reset_link):
+
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return
+
+    subject = "Reinitialistion de ton mot de passe"
+    message = render_to_string("accounts/emails/password_reset_email.txt", {"username" : user.username, "reset_link" : reset_link})
+
+    try:
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
+    except Exception as exc:
+        raise self.retry(exc=exc)
