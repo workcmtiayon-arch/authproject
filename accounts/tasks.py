bebf592fb57934d229cda2c344575dcb261ef704
Malik <workcmtiayon@gmail.com>
2026-08-13 17:26:24 +0100

    refactor: apply DRY by centralizing shared email sending and retry logic (accounts/tasks.py)

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -6,6 +6,18 @@ from django.template.loader import render_to_string
 
 User = get_user_model()
 
+def _send_email(self, subject, message, user):
+    try:
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
+    except Exception as exc:
+        raise self.retry(exc=exc)
+
 @shared_task(bind=True, max_retries=3, default_retry_delay=60)
 def send_activation_email_task(self, user_id, activation_link):
 
@@ -17,16 +29,7 @@ def send_activation_email_task(self, user_id, activation_link):
     subject = "Confirmez votre adresse Email"
     message = render_to_string("accounts/emails/activation_email.txt", {"username" : user.username, "activation_link": activation_link})
 
-    try:
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[user.email],
-            fail_silently=False,
-        )
-    except Exception as exc:
-        raise self.retry(exc=exc)
+    _send_email(self, subject, message, user)
 
     
 
@@ -41,13 +44,4 @@ def send_password_reset_email_task(self, user_id, reset_link):
     subject = "Reinitialistion de ton mot de passe"
     message = render_to_string("accounts/emails/password_reset_email.txt", {"username" : user.username, "reset_link" : reset_link})
 
-    try:
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[user.email],
-            fail_silently=False,
-        )
-    except Exception as exc:
-        raise self.retry(exc=exc)
+    _send_email(self, subject, message, user)
